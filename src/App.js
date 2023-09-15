74c943f141b8f7f48c6b6b437d83a48d07c7ed3b
gunmentoy <tomnguyen0443@gmail.com>
2023-09-14 20:10:41 -0700

    Second day of making task tracker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
